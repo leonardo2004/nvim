dd9e02a5c6c4633fc87a6fb991b0bdc905697686
leonardo2004 <dminer3x@gmail.com>
2026-01-27 01:21:27 -0300

    Markdown support

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -5,17 +5,14 @@ return {
 			require("mason").setup()
 		end,
 	},
-
 	{
 		"williamboman/mason-lspconfig.nvim",
 		dependencies = {
-			"williamboman/mason.nvim",
 			"neovim/nvim-lspconfig",
 			"hrsh7th/cmp-nvim-lsp",
 		},
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
 			require("mason-lspconfig").setup({
 				handlers = {
 					function(server_name)
@@ -27,12 +24,10 @@ return {
 			})
 		end,
 	},
-
 	{
 		"jay-babu/mason-nvim-dap.nvim",
 		event = "VeryLazy",
 		dependencies = {
-			"williamboman/mason.nvim",
 			"mfussenegger/nvim-dap",
 		},
 		opts = {
